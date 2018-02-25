38a4e92c3a83d8198a443d86b5c37bd6e481c668
Bala Chandrasekaran <mouli716@gmail.com>
2018-02-24 18:20:56 -0600

    finished tsak 2

--- Homework3/hw3_main.py
+++ Homework3/hw3_main.py
@@ -23,12 +23,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
 import matplotlib.pyplot as plt
-import cPickle
+import _pickle as cPickle
 import numpy as np
+from scipy.spatial import distance
+import skbio
+import pandas as pd
 
 def unpickle(file):
     with open(file, 'rb') as fo:
-        dict = cPickle.load(fo)
+        dict = cPickle.load(fo, encoding = 'Latin1')
     return dict
 
 with open('cifar-10-batches-py/batches.meta', 'rb') as fo:
@@ -66,8 +69,7 @@ for i in range(10):
     for j in range(3):
         mean_img_dict[i][j] = mean_img_dict[i][j]/num_images
 
-print(mean_img_dict)
-
+# print('image means', mean_img_dict)
 
 # Sorting the images by category (label)
 sorted_imgs = [[] for i in range(num_labels)]
@@ -93,7 +95,26 @@ for var1 in vars1_arr:
     plt.plot(var1)
 
 plt.legend([str(i) for i in range(10)], loc='best')
-plt.show()
+# plt.show()
+
+dist_matrix = np.zeros((10, 10))
+# print(dist_matrix)
+
+for i in range(num_labels):
+  for j in range(num_labels):
+    if (j > i):
+      first_img, second_img = mean_img_dict[i], mean_img_dict[j]
+      # print('1', first_img, '2', second_img)
+      dist_matrix[i][j] = distance.euclidean(first_img, second_img)
+print(dist_matrix)
+
+dist_mat_df = pd.DataFrame(dist_matrix)
+Ar_dist = distance.squareform(distance.pdist(dist_mat_df.T))
+DM_dist = skbio.stats.distance.DistanceMatrix(Ar_dist)
+PCoA = skbio.stats.ordination.pcoa(DM_dist)
+PCoA.plot(df=dist_mat_df, column='distances')
+
+
 
 
 
