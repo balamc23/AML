Fixed the error regarding mean image calculation

Homework3/hw3_main.py
@@ -54,21 +54,25 @@ mean_img_dict = dict()
 for label, rbg in labels_rbgs:
     mean_img_dict[label] = rbg
 
-print(len(mean_img_dict[0]))
-print(len(first_set['data'][0]))
-print(mean_img_dict)
+# print(len(mean_img_dict[0]))
+# print(len(first_set['data'][0]))
+# print(mean_img_dict)
+print("=========")
+print(mean_img_dict[0])
+print("=========")
 
 for i in range(num_images):
     label = first_set['labels'][i]
     for j in range(num_pixels):
-        print(j)
-        mean_img_dict[label][i] += first_set['data'][i][j]
+        mean_img_dict[label][j] += first_set['data'][i][j]
 
 print(mean_img_dict)
 
 for i in range(num_labels):
     for j in range(num_pixels):
         mean_img_dict[i][j] = mean_img_dict[i][j]/num_images
+
+print(mean_img_dict)
 
 # print('image means', mean_img_dict)
 
@@ -100,22 +104,22 @@ plt.legend([str(i) for i in range(10)], loc='best')
 
 
 # Task 2 below
-dist_matrix = np.zeros((10, 10))
+# dist_matrix = np.zeros((10, 10))
+# # print(dist_matrix)
+#
+# for i in range(num_labels):
+#   for j in range(num_labels):
+#     if (j > i):
+#       first_img, second_img = mean_img_dict[i], mean_img_dict[j]
+#       # print('1', first_img, '2', second_img)
+#       dist_matrix[i][j] = distance.euclidean(first_img, second_img)
 # print(dist_matrix)
-
-for i in range(num_labels):
-  for j in range(num_labels):
-    if (j > i):
-      first_img, second_img = mean_img_dict[i], mean_img_dict[j]
-      # print('1', first_img, '2', second_img)
-      dist_matrix[i][j] = distance.euclidean(first_img, second_img)
-print(dist_matrix)
-
-dist_mat_df = pd.DataFrame(dist_matrix)
-Ar_dist = distance.squareform(distance.pdist(dist_mat_df.T))
-DM_dist = skbio.stats.distance.DistanceMatrix(Ar_dist)
-PCoA = skbio.stats.ordination.pcoa(DM_dist)
-PCoA.plot(df=dist_mat_df, column='distances')
+#
+# dist_mat_df = pd.DataFrame(dist_matrix)
+# Ar_dist = distance.squareform(distance.pdist(dist_mat_df.T))
+# DM_dist = skbio.stats.distance.DistanceMatrix(Ar_dist)
+# PCoA = skbio.stats.ordination.pcoa(DM_dist)
+# PCoA.plot(df=dist_mat_df, column='distances')
 
 
 